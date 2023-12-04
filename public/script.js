662c8111c32bddecfb8facca9ced78ea40356a19
v_ch <mracdorac@gmail.com>
2023-12-04 22:23:21 +0100

    ...

--- public/script.js
+++ public/script.js
@@ -1,36 +1,32 @@
 fetch('https://mock.shop/api?query=%7B%20products(first%3A%2020)%20%7B%20edges%20%7B%20node%20%7B%20id%20title%20description%20featuredImage%20%7B%20id%20url%20%7D%20variants(first%3A%203)%20%7B%20edges%20%7B%20node%20%7B%20price%20%7B%20amount%20currencyCode%20%7D%20%7D%20%7D%20%7D%20%7D%20%7D%20%7D%7D'
     )
-    .then(res => {
-        if (!res.ok) {
-            return Promise.reject('4xx or 5xx problem');
-        }
-        
-        return res.json();
-    })
-
+    .then(res => res.json())
+    
     .then(data => {
         
-        console.log(data.data.products.edges); 
+        console.log(data.data.products.edges[1].node.variants.edges[1].node.price.amount); 
+        console.log(data.data.products.edges[1].node.variants.edges[1].node.price.currencyCode);
 
-
-        const html = data.data.products.edges.map(product => {
+        const products = data.data.products.edges.map(product => {
             return `
             <div class="items">
                 <img src="${product.node.featuredImage.url}" alt="${product.title}"/>
                 
                 <div class="content">
                     <p>${product.node.title} </p>
-                    <p></p>
+                    <p>${product.node.variants.edges.node}</p>
                 </div>
+
+
 
             </div>
             `
         })
         .join("");
 
-        console.log(html);
+        console.log(products);
         document
-        .querySelector("#products").insertAdjacentHTML("afterbegin", html)
+        .querySelector("#products").insertAdjacentHTML("afterbegin", products)
 
     })
 
