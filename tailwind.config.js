f9012ac0072245dea492aa83aa3c90df46ff8652
v_ch <mracdorac@gmail.com>
2023-12-27 21:31:10 +0100

    finish created template e-commerce

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,21 @@ module.exports = {
     extend: {
       spacing: {
         '95%': '95%',
+        '5%': '5%',
       },
       colors: {
         primary: '#459068',
         secondary: '#377DFF',
         neutral: '#141718',
         grayLight: '#F3F5F7',
+        grayText: '#E8ECEF',
       },
       borderWidth: {
         1: '1px',
-      }
+      },
+      borderColor: {
+        grayLight: '#F3F5F7',
+      },
 
     },
   },
